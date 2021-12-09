2de862fb7407d5590d73f2a3054a26cf51b8d592
Lilly Daniell <daniell.lilly@gmail.com>
2021-12-09 12:49:04 +0000

    Refactor feature test to include model class method #create

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,11 +1,9 @@
 feature 'Viewing bookmarks' do
   scenario 'visiting the index page' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    # Add the test data
-    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.channel4.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.netflix.com');")
-    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.bbc.co.uk/iplayer');")
+    Bookmark.create(url: 'https://www.channel4.com')
+    Bookmark.create(url: 'https://www.netflix.com')
+    Bookmark.create(url: 'https://www.bbc.co.uk/iplayer')
 
     visit('/bookmarks')
     expect(page).to have_content 'https://www.channel4.com'
